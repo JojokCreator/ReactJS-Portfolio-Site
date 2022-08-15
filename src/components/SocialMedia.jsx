ac465d5ae2976d1867d105588c0c6d4f038bea92
Joe Klakus <joeyk2k@hotmail.com>
2022-08-15 14:23:30 +0100

    updated social icons

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -5,13 +5,19 @@ import { FaFacebookF } from 'react-icons/fa';
 const SocialMedia = () => (
   <div className="app__social">
     <div>
+      <a href="https://github.com/JojokCreator">
       <BsGithub />
+      </a>
     </div>
     <div>
+    <a href="https://www.facebook.com/public/Joe-Klakus">
       <FaFacebookF />
+      </a>
     </div>
     <div>
+    <a href="https://www.linkedin.com/joe-klakus-40bb9820b%2F">
       <BsLinkedin />
+      </a>
     </div>
   </div>
 );
