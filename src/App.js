a7ce71b00010f012cab4e5633dbd5d085a78303d
Joe Klakus <joeyk2k@hotmail.com>
2022-12-17 17:26:16 +0200

    added analytics

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import  './components/DarkToggle/DarkToggle.scss';
 import { About, Footer, Header, Skills, Work } from './container';
 import { Navbar } from './components';
 import './App.scss'
+import ReactGA from 'react-ga';
+
+  const TRACKING_ID = "UA-213987738-2"; // OUR_TRACKING_ID
+  ReactGA.initialize(TRACKING_ID);
 
 const App = () => {
   return (
