1a40974e99ad0b486c3bc573c0577e7533082006
Joe Klakus <joeyk2k@hotmail.com>
2023-02-16 16:30:47 +0800

    added testimonials

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react'
-import  './components/DarkToggle/DarkToggle.scss';
-import { About, Footer, Header, Skills, Work } from './container';
+import React from 'react';
+import './components/DarkToggle/DarkToggle.scss';
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
 import { Navbar } from './components';
-import './App.scss'
-import ReactGA from "react-ga4";
+import './App.scss';
+import ReactGA from 'react-ga4';
 
-ReactGA.initialize("G-WGVTW9T3C0");
-ReactGA.send("pageview");
+ReactGA.initialize('G-WGVTW9T3C0');
+ReactGA.send('pageview');
 
 const App = () => {
   return (
     <div className='app'>
-    <Navbar />
-    <Header />
-    <About />
-    <Work />
-    <Skills />
-    <Footer />
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,43 +7,51 @@ import DarkToggle from '../DarkToggle/DarkToggle';
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
-  const handleOnClick = e => {
+  const handleOnClick = (e) => {
     e.stopPropagation();
     setToggle(!toggle);
   };
 
   return (
-    <nav className="app__navbar">
-      <div className="app__navbar-logo">
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
         <h1>{'</> Joe Klakus'}</h1>
       </div>
-      <ul className="app__navbar-links">
-        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
-          <li className="app__flex p-text" key={`link-${item}`}>
-            <div />
-            <a href={`#${item}`}>{item}</a>
-          </li>
-        ))}
+      <ul className='app__navbar-links'>
+        {['home', 'about', 'work', 'skills', 'testimonials', 'contact'].map(
+          (item) => (
+            <li className='app__flex p-text' key={`link-${item}`}>
+              <div />
+              <a href={`#${item}`}>{item}</a>
+            </li>
+          ),
+        )}
       </ul>
 
-      <div className="app__navbar-menu">
-      <HiMenuAlt4 onClick={handleOnClick} />
+      <div className='app__navbar-menu'>
+        <HiMenuAlt4 onClick={handleOnClick} />
 
         {toggle && (
           <motion.div
-          initial={{ width: 0 }}
-          animate={{ width: 300 }}
-          transition={{ duration: 0.85, ease: 'easeOut' }}
+            initial={{ width: 0 }}
+            animate={{ width: 300 }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}
           >
             <HiX onClick={() => setToggle(false)} />
             <motion.span
               initial={{ width: 0 }}
               animate={{ width: 70 }}
-              transition={{ duration: 0.85, ease: "easeOut" }}
-            >
-            </motion.span>
+              transition={{ duration: 0.85, ease: 'easeOut' }}
+            ></motion.span>
             <ul>
-              {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+              {[
+                'home',
+                'about',
+                'work',
+                'skills',
+                'testimonials',
+                'contact',
+              ].map((item) => (
                 <li key={item}>
                   <a href={`#${item}`} onClick={() => setToggle(false)}>
                     {item}
@@ -54,7 +62,7 @@ const Navbar = () => {
           </motion.div>
         )}
       </div>
-        <DarkToggle />
+      <DarkToggle />
     </nav>
   );
 };
